7b17dc00e2e825e93d1745e6169732694c78a9bb
Andrea Wu <andrea.wzg@gmail.com>
2024-02-26 14:52:18 -0800

    add 3D animation and images to plans and details section

--- src/pages/components/Details/index.tsx
+++ src/pages/components/Details/index.tsx
@@ -1,4 +1,5 @@
-import styles from './Details.module.css'
+import styles from './Details.module.css';
+import Spline from '@splinetool/react-spline';
 
 export default function Details() {
   return (
@@ -6,12 +7,14 @@ export default function Details() {
       <p className={styles.title}>Enjoy a personalized listening experience tailored to your tastes</p>
       <div className={styles.cardsContainer}>
         <div className={styles.card}>
+          <Spline style={{height: 130, width: 130}} scene="https://prod.spline.design/6onI15xmywHu1nT1/scene.splinecode" />
           <p className={styles.subtitle}>Homegrown Melodies</p>
           <p className={styles.description}>
             From soulful ballads to energetic beats, discover the diverse sounds that define your city's unique musical landscape
           </p>
         </div>
         <div className={styles.card}>
+          <Spline style={{height: 130, width: 130}} scene="https://prod.spline.design/LupA7TVgxdSFKboK/scene.splinecode" />
           <p className={styles.subtitle}>City Canvas</p>
           <p className={styles.description}>
             From colorful murals to striking sculptures, discover the 
@@ -20,6 +23,7 @@ export default function Details() {
           </p>
         </div>
         <div className={styles.card}>
+          <Spline style={{height: 130, width: 130}} scene="https://prod.spline.design/t28EqB5It0AIFnar/scene.splinecode" />
           <p className={styles.subtitle}>Explore hidden gems</p>
           <p className={styles.description}>
             Learn about local artists, and gain a deeper appreciation for the artistic tapestry woven throughout your community
--- src/pages/components/Plans/index.tsx
+++ src/pages/components/Plans/index.tsx
@@ -1,4 +1,6 @@
-import styles from "./Plans.module.css"
+import styles from "./Plans.module.css";
+import Link from "next/link";
+import Spline from '@splinetool/react-spline';
 
 export default function Plans() {
   return (
@@ -11,6 +13,54 @@ export default function Plans() {
           needs. Whether you're a casual listener or a dedicated art enthusiast, 
           we have the perfect plan for you
         </p>
+        <div className={styles.plansDisplay}>
+          <div className={styles.plansCard}>
+            <Spline style={{height: 165}} scene="https://prod.spline.design/ZPo9RcfQQdrQ3oWJ/scene.splinecode" />
+            <Link className={styles.links} href="#">Individual</Link>
+            <div className={styles.cardInfo}>
+              <p className={styles.planPrice}>$5.99 /<span style={{fontSize: "var(--regular-font)", fontWeight: "var(--regular-bold)"}}>month</span></p>
+              <p style={{textAlign: "center"}}>1 account</p>
+              <div className={styles.planInfo}>
+                <p>• Listen to music ad-free</p>
+                <p>• Early access to new releases from featured artists</p>
+                <p>• Play off-line</p>
+                <p>• Create unlimited playlist</p>
+                <p>• Unlimited shuffle</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.plansCard}>
+            <Spline style={{height: 165}} scene="https://prod.spline.design/zgBJ9tpFn2BQf9Uw/scene.splinecode" />
+            <Link className={styles.links} href="#">Duo</Link>
+            <div className={styles.cardInfo}>
+              <p className={styles.planPrice}>$8.99 /<span style={{fontSize: "var(--regular-font)", fontWeight: "var(--regular-bold)"}}>month</span></p>
+              <p style={{textAlign: "center"}}>2 accounts</p>
+              <div className={styles.planInfo}>
+                <p>• Listen to music ad-free</p>
+                <p>• Early access to new releases from featured artists</p>
+                <p>• Play off-line</p>
+                <p>• Create unlimited playlist</p>
+                <p>• Unlimited shuffle</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.plansCard}>
+            {/* <Spline scene="https://prod.spline.design/sKJiO8UJ0jGtwdQH/scene.splinecode" /> */}
+            <Spline style={{height: 165}} scene="https://prod.spline.design/sKJiO8UJ0jGtwdQH/scene.splinecode" />
+            <Link className={styles.links} href="#">Family</Link>
+            <div className={styles.cardInfo}>
+              <p className={styles.planPrice}>$10.99 /<span style={{fontSize: "var(--regular-font)", fontWeight: "var(--regular-bold)"}}>month</span></p>
+              <p style={{textAlign: "center"}}>4 accounts</p>
+              <div className={styles.planInfo}>
+                <p>• Listen to music ad-free</p>
+                <p>• Early access to new releases from featured artists</p>
+                <p>• Play off-line</p>
+                <p>• Create unlimited playlist</p>
+                <p>• Unlimited shuffle</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
